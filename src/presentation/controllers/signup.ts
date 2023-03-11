3dfe072058902084ccc42f5bc1ef88d1682d0651
Micael Oliveira <micaelmoura.oliveira@gmail.com>
2023-03-11 01:24:13 -0300

    refactor: fix wrong commit

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,4 +1,5 @@
 import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/http-helper'
 import { type HttpRequest, type HttpResponse } from '../protocols/http'
 
 export class SignUpController {
